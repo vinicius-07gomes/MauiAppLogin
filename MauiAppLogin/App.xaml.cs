aa384ccd7393ebb148b0a121cdb3010b9788e3ad
vinicius-07gomes <vinicius_07gomes@hotmail.com>
2024-10-27 20:51:07 -0300

    Finalização do Projeto Login

--- MauiAppLogin/App.xaml.cs
+++ MauiAppLogin/App.xaml.cs
@@ -5,6 +5,21 @@
         public App()
         {
             InitializeComponent();
+
+            string? usuario_logado = null;
+
+            Task.Run(async () =>
+            {
+                usuario_logado = await SecureStorage.Default.GetAsync("usuario_logado");
+                if(usuario_logado == null)
+                {
+                    MainPage = new Login();
+                } else 
+                {
+                    MainPage = new Protegida();
+                }           
+            });
+
             MainPage = new Login();
         }
         protected override Window CreateWindow(IActivationState activationState)
--- MauiAppLogin/Protegida.xaml.cs
+++ MauiAppLogin/Protegida.xaml.cs
@@ -2,8 +2,27 @@ namespace MauiAppLogin;
 
 public partial class Protegida : ContentPage
 {
-	public Protegida()
-	{
-		InitializeComponent();
-	}
+    public Protegida()
+    {
+        InitializeComponent();
+        string? usuario_logado = null;
+
+        Task.Run(async () =>
+        {
+            usuario_logado = await SecureStorage.Default.GetAsync("usuario_logado");
+            lbl_boasvindas.Text = $"Bem-vindo(a) {usuario_logado}";
+        });
+    }
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        bool confirmacao = await DisplayAlert("Tem Certeza?", "Sair do App?", "Sim", "Não");
+        if (confirmacao)
+        {
+            SecureStorage.Default.Remove("usuario_logado");
+            App.Current.MainPage = new Login();
+        }
+
+        
+    }
 }
